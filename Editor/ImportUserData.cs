7e26b02e3d0cea7100ba65067a16d98740d15cfd
Jeiel Aranal <jeiel.aranal@gmail.com>
2017-05-31 23:30:33 +0800

    Added import user data

---
+++ Editor/ImportUserData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace SubjectNerd.PsdImporter
+{
+	public enum ScaleFactor
+	{
+		Full,
+		Half,
+		Quarter
+	}
+
+	[Serializable]
+	public struct ImportLayerData
+	{
+		public string pathId;
+		public string name;
+		public SpriteAlignment Alignment;
+		public Vector2 Pivot;
+		public ScaleFactor ScaleFactor;
+
+		public ImportLayerData[] ChildLayers;
+	}
+
+	[Serializable]
+	public class ImportUserData
+	{
+		public string PackingTag;
+		public string TargetDirectory;
+		public SpriteAlignment DefaultAlignment = SpriteAlignment.Center;
+		public Vector2 DefaultPivot = new Vector2(0.5f, 0.5f);
+		public ScaleFactor ScaleFactor = ScaleFactor.Full;
+
+	}
+}
