00861734baba4719ea22c01efbdbcbf75ba1421f
Jeiel Aranal <jeiel.aranal@gmail.com>
2017-06-02 01:28:54 +0800

    Added FullSerializer

---
+++ Editor/FullSerializer/fsIgnoreAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SubjectNerd.PsdImporter.FullSerializer {
+    /// <summary>
+    /// The given property or field annotated with [JsonIgnore] will not be
+    /// serialized.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct)]
+    public sealed class fsIgnoreAttribute : Attribute {
+    }
+}
